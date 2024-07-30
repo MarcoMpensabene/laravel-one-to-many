b72ce752ee62887ff9a353ed179d1ae578dff5ef
MarcoMpensabene <marcompensabene@gmail.com>
2024-07-30 16:56:38 +0200

    added relation for type and project on model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Type;
 
 class Project extends Model
 {
@@ -18,4 +19,9 @@ class Project extends Model
         'image_url',
         'stack'
     ];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,12 +5,14 @@
     <div class="container">
         <div class="row justify-content-center">
             <h1>Project  Index </h1>
+            @dd($project->type->name)
             <div class="container">
                 <div class="row">
                     <article class="col-12">
                         <div class="card">
                             <div class="card-body">
                                 <h5 class="card-title">Project : {{$project->title}}</h5>
+                                <h4 class="card-title">Type : {{$project->type->name}}</h4>
                                 <p class="card-text">Description : {{$project->description}}</p>
                                 <p class="card-text"><small class="text-body-secondary">Autore : {{$project->author}}</small></p>
                                 <p class="card-text">Stack : {{$project->stack}}</p>
